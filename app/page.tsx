6ac7a21e152cf1e8552e0a221fa08f5dce10eb4d
Fergulati <demetrick@status.im>
2024-02-02 20:54:40 -0500

    modified app/pages.tst

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,7 @@
 import { getFrameMetadata } from '@coinbase/onchainkit';
 import type { Metadata } from 'next';
+import Head from 'next/head';
+
 
 const NEXT_PUBLIC_URL = 'https://zizzamia.xyz';
 
@@ -14,10 +16,10 @@ const frameMetadata = getFrameMetadata({
 });
 
 export const metadata: Metadata = {
-  title: 'zizzamia.xyz',
+  title: 'Hashing It Out Sensei',
   description: 'LFG',
   openGraph: {
-    title: 'zizzamia.xyz',
+    title: 'Hashing It Out Sensei',
     description: 'LFG',
     images: [`${NEXT_PUBLIC_URL}/park-1.png`],
   },
@@ -28,15 +30,12 @@ export const metadata: Metadata = {
 
 export default function Page() {
   return (
-    <>
-    <!DOCTYPE html>
-    <html>
+   <>
       <head>
-      <meta property="fc:frame" content="vNext" />
-      <meta property="fc:frame:image" content="http://...image-question.png" />
-      <meta property="fc:frame:button:1" content="https://media.discordapp.net/attachments/1086277219749474424/1202079543272149052/snail_with_a_hat_japanese_scroll_ink_1.png?ex=65cc26d0&is=65b9b1d0&hm=0cc4d81ac64aeb1a2c4d61df51e4233862c002c11b13eed93291f6a25fcbcded&=&format=webp&quality=lossless&width=2422&height=1275" />
+        <meta property="fc:frame" content="vNext" />
+        <meta property="fc:frame:image" content="https://media.discordapp.net/attachments/1086277219749474424/1202079543272149052/snail_with_a_hat_japanese_scroll_ink_1.png?ex=65cc26d0&is=65b9b1d0&hm=0cc4d81ac64aeb1a2c4d61df51e4233862c002c11b13eed93291f6a25fcbcded&=&format=webp&quality=lossless&width=2422&height=1275" />
+        <meta property="fc:frame:button:1" content="https://media.discordapp.net/attachments/1086277219749474424/1202079543272149052/snail_with_a_hat_japanese_scroll_ink_1.png?ex=65cc26d0&is=65b9b1d0&hm=0cc4d81ac64aeb1a2c4d61df51e4233862c002c11b13eed93291f6a25fcbcded&=&format=webp&quality=lossless&width=2422&height=1275" />
       </head>
-    </html>
-    </>
+   </>
   );
 }
