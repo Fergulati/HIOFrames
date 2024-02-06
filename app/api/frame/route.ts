38a13e8c4d1f6ac4b2176bdeb3076913174fea5c
Fergulati <demetrick@status.im>
2024-02-05 22:47:32 -0500

    Update

--- app/api/frame/route.ts
+++ app/api/frame/route.ts
@@ -1,7 +1,8 @@
 import { FrameRequest, getFrameMessage, getFrameHtmlResponse } from '@coinbase/onchainkit';
 import { NextRequest, NextResponse } from 'next/server';
 
-const NEXT_PUBLIC_URL = 'https://zizzamia.xyz';
+const NEXT_PUBLIC_URL = 'https://press.logos.co/podcasts/hashing-it-out';
+const SNAIL_PIC_URL = 'https://media.discordapp.net/attachments/1086277219749474424/1202079543272149052/snail_with_a_hat_japanese_scroll_ink_1.png?ex=65cc26d0&is=65b9b1d0&hm=0cc4d81ac64aeb1a2c4d61df51e4233862c002c11b13eed93291f6a25fcbcded&=&format=webp&quality=lossless&width=2422&height=1275'
 
 async function getResponse(req: NextRequest): Promise<NextResponse> {
   let accountAddress: string | undefined = '';
@@ -20,8 +21,8 @@ async function getResponse(req: NextRequest): Promise<NextResponse> {
           label: `🌲 ${accountAddress} 🌲`,
         },
       ],
-      image: `${NEXT_PUBLIC_URL}/park-2.png`,
-      post_url: `${NEXT_PUBLIC_URL}/api/frame`,
+      image: `${SNAIL_PIC_URL}`,
+      post_url: `${NEXT_PUBLIC_URL}`,
     }),
   );
 }
