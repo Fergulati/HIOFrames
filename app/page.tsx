4c8641800f1208ce5c9ff4a12af41205f3bad5d4
Fergulati <demetrick@status.im>
2024-02-05 21:22:35 -0500

    Added a redirect

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import type { Metadata } from 'next';
 import Head from 'next/head';
 
 
-const NEXT_PUBLIC_URL = 'c';
+const NEXT_PUBLIC_URL = 'https://press.logos.co/podcasts/hashing-it-out'
 const SNAIL_PIC_URL = 'https://media.discordapp.net/attachments/1086277219749474424/1202079543272149052/snail_with_a_hat_japanese_scroll_ink_1.png?ex=65cc26d0&is=65b9b1d0&hm=0cc4d81ac64aeb1a2c4d61df51e4233862c002c11b13eed93291f6a25fcbcded&=&format=webp&quality=lossless&width=2422&height=1275'
 
 const frameMetadata = getFrameMetadata({
@@ -32,6 +32,9 @@ export const metadata: Metadata = {
 export default function Page() {
   return (
    <>
+      <Head>
+      <meta http-equiv = "refresh" content = "0; url = https://press.logos.co/podcasts/hashing-it-out" />
+      </Head>
       <h1>"Hashing It Out Sensei"
         <meta property="fc:frame" content="vNext" />
         <meta property="fc:frame:image" content="https://media.discordapp.net/attachments/1086277219749474424/1202079543272149052/snail_with_a_hat_japanese_scroll_ink_1.png?ex=65cc26d0&is=65b9b1d0&hm=0cc4d81ac64aeb1a2c4d61df51e4233862c002c11b13eed93291f6a25fcbcded&=&format=webp&quality=lossless&width=2422&height=1275" />
