31760863b16471a12285bbf725a1c106cf336c50
Fergulati <demetrick@status.im>
2024-02-05 21:12:54 -0500

    Add hyperlink to webpage

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import type { Metadata } from 'next';
 import Head from 'next/head';
 
 
-const NEXT_PUBLIC_URL = 'https://press.logos.co/podcasts/hashing-it-out';
+const NEXT_PUBLIC_URL = 'c';
 const SNAIL_PIC_URL = 'https://media.discordapp.net/attachments/1086277219749474424/1202079543272149052/snail_with_a_hat_japanese_scroll_ink_1.png?ex=65cc26d0&is=65b9b1d0&hm=0cc4d81ac64aeb1a2c4d61df51e4233862c002c11b13eed93291f6a25fcbcded&=&format=webp&quality=lossless&width=2422&height=1275'
 
 const frameMetadata = getFrameMetadata({
@@ -37,6 +37,7 @@ export default function Page() {
         <meta property="fc:frame:image" content="https://media.discordapp.net/attachments/1086277219749474424/1202079543272149052/snail_with_a_hat_japanese_scroll_ink_1.png?ex=65cc26d0&is=65b9b1d0&hm=0cc4d81ac64aeb1a2c4d61df51e4233862c002c11b13eed93291f6a25fcbcded&=&format=webp&quality=lossless&width=2422&height=1275" />
         <meta property="fc:frame:button:1" content="Have A Listen" />
       </h1>
+      <h2><a href="https://press.logos.co/podcasts/hashing-it-out">Click here to visit the Hashing It Out Webpage</a></h2>
    </>
   );
 }
